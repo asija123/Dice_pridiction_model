bb6bed892514ae5e7ada75e1340cc73d00d832fb
asija123 <lakshayasija36@gmail.com>
2025-04-16 00:22:06 +0530

    Add gunicorn to requirements.txt for Railway

--- app.py
+++ app.py
@@ -41,7 +41,7 @@
 #     app.run(debug=True)
 
 # ***********************************************************************************
-
+import os
 from flask import Flask, render_template, request
 from predictor import predict_next_sums, save_actual_result
 
@@ -69,4 +69,5 @@ def index():
 
     return render_template('index.html', prediction=last_prediction, message=message)
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
